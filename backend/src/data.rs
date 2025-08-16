cafb717b82332a2e4fce91e6a3362abb2bdc55b8
Neel <ntamtam614@gmail.com>
2025-08-16 10:46:40 -0500

    Started implementation for multi-file kanji vectorization using generic iterator of AsRef<std::path::Path>.

--- backend/src/data.rs
+++ backend/src/data.rs
@@ -1,6 +1,6 @@
 use csv::{Reader, StringRecord};
-use std::error::Error;
 use std::fs::File;
+use std::{error::Error, path::Path};
 
 pub fn vectorize_word_list(path: &str) -> Result<Vec<String>, Box<dyn Error>> {
     let file: File = File::open(path)?;
@@ -16,15 +16,21 @@ pub fn vectorize_word_list(path: &str) -> Result<Vec<String>, Box<dyn Error>> {
     Ok(word_list)
 }
 
-pub fn vectorize_joyo_kanji(path: &str) -> Result<Vec<String>, Box<dyn Error>> {
-    let file: File = File::open(path)?;
-    let mut rdr: Reader<File> = Reader::from_reader(file);
+pub fn vectorize_joyo_kanji<I, P>(paths: I) -> Result<Vec<String>, Box<dyn Error>>
+where
+    I: IntoIterator<Item = P>,
+    P: AsRef<Path>,
+{
     let mut kanji_list = Vec::new();
 
-    for result in rdr.records() {
-        let record: StringRecord = result?;
-        let kanji: &str = record.get(0).unwrap_or("N/A");
-        kanji_list.push(kanji.to_string());
+    for path in paths {
+        let mut rdr: Reader<_> = Reader::from_path(path)?;
+        for result in rdr.records() {
+            let record: StringRecord = result?;
+            if let Some(kanji) = record.get(0) {
+                kanji_list.push(kanji.to_owned());
+            }
+        }
     }
 
     Ok(kanji_list)
